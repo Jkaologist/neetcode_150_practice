1891fc03f6fd74c40176814edf69d4599527c845
jkaologist <jon.kao.dev@gmail.com>
2024-04-11 08:25:55 -0700

    Comprehensions.py updates

--- set_comprehensions.py
+++ set_comprehensions.py
@@ -1,8 +1,11 @@
 nums = [1,2,2,3,3,3,4,5,5,5,6]
-
+nums_even = [num for num in nums if num % 2 == 0]
 unique_uses_a_lot_of_memory = set([x**2 for x in nums])
 unique_fast = set(x**2 for x in nums)
 unique_fastest = {x**2 for x in nums}
+
+print(nums_even)
+print("ORIGINAL = ", id(nums))
 
 print(unique_uses_a_lot_of_memory)
 print(type(unique_uses_a_lot_of_memory))
