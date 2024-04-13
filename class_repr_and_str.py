34bba37a94fae85e214240f3731264c2aefd24b9
jkaologist <jon.kao.dev@gmail.com>
2024-04-13 09:44:49 -0700

    Setting defaults on objects

---
+++ class_repr_and_str.py
@@ -0,0 +1,52 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.isWord = False
+    def __repr__(self):  
+        return "TrieNode children are:% s isWord:% s" % (self.children, self.isWord)  
+    def __str__(self):  
+        return "From str method of TrieNode: children are % s, " \
+              "isWord is % s" % (self.children, self.isWord)
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    def insert(self, word: str) -> None:
+        curr = self.root
+
+        for char in word:
+            i = ord(char) - ord("a")
+            if curr.children[i] == None:
+                curr.children[i] = TrieNode()
+            curr = curr.children[i]
+
+        curr.isWord = True
+    def search(self, word: str) -> bool:
+        curr = self.root
+
+        for char in word:
+            i = ord(char) - ord("a")
+            if curr.children[i] == None:
+                return False
+            curr = curr.children[i]
+        return curr.isWord
+
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+
+        for char in prefix:
+            i = ord(char) - ord("a")
+            if curr.children[i] == None:
+                return False
+            curr = curr.children[i]
+
+        return True
+
+# Driver Code          
+root = TrieNode()
+  
+# This calls __str__()  
+print(root)  
+  
+# This calls __repr__()
+print([root]) 
---
+++ set_default.py
@@ -0,0 +1,13 @@
+car = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+color_of_car_before = car.setdefault("color", "white")
+color_of_car_after = car.setdefault("brand", "Acura")
+
+
+print(car)
+
+print(color_of_car_before, color_of_car_after)
