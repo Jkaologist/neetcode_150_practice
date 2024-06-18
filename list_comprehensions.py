f2271cd4de426723653b81fca0b4112a59cfd8e4
jkaologist <jon.kao.dev@gmail.com>
2024-06-18 12:31:11 -0700

    list_comprehensions and reversals

---
+++ list_comprehensions.py
@@ -0,0 +1,34 @@
+# lst1 = [1,2,3,4,5]
+# #   i = 0,1,2,3,4
+# lst2 = [_ for _ in range(1, 6)]
+
+# print(lst1)
+# print(lst2)
+
+# lst3 = lst1[:] # list[start:end:step]
+
+# print(lst3)
+
+# lst4 = lst1[:4]
+
+# s1 = "HELLO"
+# s2 = s1[-3::-1]
+# print(s2)
+# print(lst4)
+
+# lst1 = [_ for _ in range(1, 11)]
+# print(lst1)
+
+# lst2 = lst1[1:9:2]
+# print(lst1[9])
+# print(lst2)
+str1 = "Hello World"
+str2 = " " + str1
+#     i=012345678910
+# str2 = str1[1:7]
+# print(str2)
+
+# str3 = str1[::-1]
+# print(str3)
+str4 = str2[len(str1):-1:-1]
+print(str4)
--- range_distances.py
+++ range_distances.py
@@ -1,8 +1,14 @@
-for i in range(10):
-    print(i)
+# for i in range(10): # range(start, end, step)
+#     print(i)
 
-for j in range(0, 10):
-    print(j)
+# for j in range(0, 10):
+#     print(j)
 
-for k in range(9, -1, -1):
-    print(k)
+# for l in range(0, 10, 1):
+#     print(l)
+
+# for g in range(0, 10, 2):
+#     print(g)
+
+# for k in range(9, -1, -1):
+#     print(k)
